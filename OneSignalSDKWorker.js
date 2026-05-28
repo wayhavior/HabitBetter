9c908db72491caad6a9577736e26bd85ad3dc00e
wayhavior <maccosschanel@gmail.com>
2026-05-29 02:05:34 +0700

    Delete OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++
@@ -1,5 +0,0 @@
-// OneSignalSDKWorker.js
-// 🔔 ไฟล์นี้ต้องอยู่ใน root folder ของเว็บไซต์ (เดียวกับ index.html)
-// บทบาท: ทำให้ Push Notification ทำงานแม้เมื่อปิดแอป/เบราว์เซอร์
-
-importScripts("https://cdn.onesignal.com/sdks/OneSignalSDK.js");
