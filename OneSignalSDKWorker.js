18807595b8343c166d9c224467ee206841c17bae
wayhavior <maccosschanel@gmail.com>
2026-05-28 19:44:25 +0700

    Add files via upload

---
+++ OneSignalSDKWorker.js
@@ -0,0 +1,5 @@
+// OneSignalSDKWorker.js
+// 🔔 ไฟล์นี้ต้องอยู่ใน root folder ของเว็บไซต์ (เดียวกับ index.html)
+// บทบาท: ทำให้ Push Notification ทำงานแม้เมื่อปิดแอป/เบราว์เซอร์
+
+importScripts("https://cdn.onesignal.com/sdks/OneSignalSDK.js");
