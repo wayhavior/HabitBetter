e0cb8dc4391df44b8ef38688147b8e673c361c57
wayhavior <maccosschanel@gmail.com>
2026-05-29 01:41:56 +0700

    Update OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -2,4 +2,4 @@
 // 🔔 ไฟล์นี้ต้องอยู่ใน root folder ของเว็บไซต์ (เดียวกับ index.html)
 // บทบาท: ทำให้ Push Notification ทำงานแม้เมื่อปิดแอป/เบราว์เซอร์
 
-importScripts("https://cdn.onesignal.com/sdks/OneSignalSDK.js");
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
